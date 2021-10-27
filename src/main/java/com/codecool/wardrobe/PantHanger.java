b827d240feb34bdb609f6ff2a0e90ccf5272aaf0
Gyozo55 <74531373+Gyozo55@users.noreply.github.com>
2021-10-27 05:49:48 +0200

    [#11] Create PantHanger Class put() method and refactor takeOff() method

--- src/main/java/com/codecool/wardrobe/PantHanger.java
+++ src/main/java/com/codecool/wardrobe/PantHanger.java
@@ -3,21 +3,27 @@ package com.codecool.wardrobe;
 import com.codecool.wardrobe.clothing.Clothes;
 import com.codecool.wardrobe.clothing.Clothes.ClothesType;
 
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Predicate;
 
 public class PantHanger implements Hanger<Clothes> {
 
-    private final Optional<Clothes> lowerClothesOnHanger = Optional.empty();
-    private final Optional<Clothes> upperClothesOnHanger = Optional.empty();
+    private final ArrayList<Clothes> lowerClothesOnHanger = new ArrayList<>();
+    private final ArrayList<Clothes> upperClothesOnHanger = new ArrayList<>();
 
     @Override
     public Optional<Clothes> takeOff() {
-        if(lowerClothesOnHanger.isPresent() || upperClothesOnHanger.isPresent()){
-            return upperClothesOnHanger;
+
+        if(upperClothesOnHanger.size()>0 && lowerClothesOnHanger.size()==0){
+            return Optional.ofNullable(upperClothesOnHanger.get(0));
+        }
+        else if(lowerClothesOnHanger.size()>0){
+            return Optional.ofNullable(lowerClothesOnHanger.get(0));
         }
         else {
-            return lowerClothesOnHanger;
+            return Optional.empty();
         }
     }
 
@@ -28,7 +34,12 @@ public class PantHanger implements Hanger<Clothes> {
 
     @Override
     public void put(Clothes item) {
-
+        if(item.getType() == ClothesType.SHIRT || item.getType() == ClothesType.BLOUSE){
+            upperClothesOnHanger.add(item);
+        }
+        else {
+            lowerClothesOnHanger.add(item);
+        }
     }
 
     @Override
